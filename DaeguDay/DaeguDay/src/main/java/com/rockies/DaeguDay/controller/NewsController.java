46473bb648926af407abe6439d9a93a8047a149e
Jieon <dlwldjs7544@naver.com>
2022-09-24 07:26:56 +0900

    update files

--- DaeguDay/DaeguDay/src/main/java/com/rockies/DaeguDay/controller/NewsController.java
+++ DaeguDay/DaeguDay/src/main/java/com/rockies/DaeguDay/controller/NewsController.java
@@ -1,4 +1,31 @@
 package com.rockies.DaeguDay.controller;
 
+import com.rockies.DaeguDay.domain.News;
+import com.rockies.DaeguDay.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class NewsController {
+    private final NewsService newsService;
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping("/")
+    public String test(){
+        return "home";
+    }
+
+    @GetMapping("/News")
+    public String list(Model model){
+        List<News> newsList = newsService.findNewses();
+        model.addAttribute("newses", newsList);
+        return "News/newslist1";
+    }
 }
---
+++ DaeguDay/DaeguDay/src/main/java/com/rockies/DaeguDay/domain/Category.java
@@ -0,0 +1,8 @@
+package com.rockies.DaeguDay.domain;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private int number;
+}
